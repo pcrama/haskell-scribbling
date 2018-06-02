9c6e3a7a0a969244ffae58e3700aa4c939fb84b7
Philippe Crama <pcfeb0009@gmx.com>
2018-06-02 05:38:39 +0000

    Include track number in file name

--- ripandtag/src/Shell.hs
+++ ripandtag/src/Shell.hs
@@ -8,7 +8,10 @@ where
 import Data.Char (toUpper)
 import Data.List (intercalate)
 
-import ParseRipSpec (TrackRipSpec(..))
+import ParseRipSpec
+  ( IntOrFollowing (..)
+  , TrackRipSpec(..)
+  )
 
 simplifyLatin1 :: [String]
 simplifyLatin1 = ["e\xe9", "E\xc9"]
@@ -44,6 +47,8 @@ makeSafe x =
         startWithJust (Nothing:_) = False
         startWithJust (Just _:_) = True
 
+-- Track name:
+-- artist-album-track number-title
 safeTrackName :: TrackRipSpec -> String
 safeTrackName x = makeSafe
                 $ intercalate "-"
@@ -51,4 +56,16 @@ safeTrackName x = makeSafe
                 $ filter (maybe False $ const True)
                 $ [ artist x
                   , album x
+                  , formatTrackNumber (track x) (total x)
                   , title x ]
+  where formatTrackNumber :: IntOrFollowing -> Maybe Int -> Maybe String
+        formatTrackNumber PrevPlusOne _ = Nothing -- should never happen
+        formatTrackNumber (TheInt trk) Nothing = formatWidth trk 2
+        formatTrackNumber (TheInt trk) (Just tot)
+          | tot < 10 = formatWidth trk 1
+          | tot < 100 = formatWidth trk 2
+          | otherwise = formatWidth trk 3
+        formatWidth n w =
+          let s = show n
+              l = length s
+          in Just $ if l < w then replicate (w - l) '0' ++ s else s
