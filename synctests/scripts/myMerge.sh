845789ef86feab4e73d4db8b7d167721d7104940
Philippe Crama <pcfeb0009@gmx.com>
2018-07-23 21:24:38 +0200

    Simplify merging: concentrate on 1! file case

--- synctests/scripts/myMerge.sh
+++ synctests/scripts/myMerge.sh
@@ -6,7 +6,13 @@ current2="$4"
 _new="$5"
 currentarchopt="$6"
 cleanName="$_root/$_path"
-tmpname="$cleanName.$(date --utc +%s).$(basename "$_root")"
+# Temporary simplification: the first implementation will look at the
+# synchronization of only one file and the test code looks at only one
+# conflict file per client (_root).  Note that this simplification means that
+# new conflicts may overwrite old conflict values.
+#
+# tmpname="$cleanName.$(date --utc +%s).$(basename "$_root")"
+tmpname="$cleanName.$(basename "$_root")"
 if [ -f "$tmpname" ] ; then
     echo "Conflict in $cleanName"
     exit 1
