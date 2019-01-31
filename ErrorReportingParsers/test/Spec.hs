ba192fa3fe0a5e856624dc0ddf5bc4999a76b7b8
Philippe Crama <pcfeb0009@gmx.com>
2019-01-31 22:29:30 +0100

    Fix -Wall warnings

--- ErrorReportingParsers/test/Spec.hs
+++ ErrorReportingParsers/test/Spec.hs
@@ -1,4 +1,4 @@
-import Control.Applicative (Alternative(..), some, many)
+import Control.Applicative (Alternative(..), many)
 
 import Test.Hspec
 import Test.HUnit
@@ -20,6 +20,7 @@ s_level = S.literal '(' *> fmap Many (many s_element) <* S.literal ')'
 s_element :: S.Parser Char Nesting
 s_element = s_char <|> s_level
 
+s_parseNest :: S.Parser Char [Nesting]
 s_parseNest = many s_element
 
 s_p :: S.Parser a (a, a)
@@ -28,6 +29,9 @@ s_p = (,) <$> S.item <*> S.item
 s_q :: Eq a => a -> S.Parser a (a, a)
 s_q x = S.item >>= (\y -> if x == y then return (x, x) else fmap ((,) y) S.item)
 
+testSimpleParser :: (Show a, Show t, Eq a, Eq t)
+                 => [Char] -> S.Parser t a -> [t] -> Maybe ([t], a)
+                 -> Assertion
 testSimpleParser s p ts expected =
   let parsed = S.getParser p ts
   in assertEqual (s ++ " " ++ show ts ++ " = " ++ (show $ parsed))
@@ -44,6 +48,7 @@ mt_level = M1.literal '(' *> fmap Many (many mt_element) <* M1.literal ')'
 mt_element :: M1.Parser Char Nesting
 mt_element = mt_char <|> mt_level
 
+mt_parseNest :: M1.Parser Char [Nesting]
 mt_parseNest = many mt_element
 
 mt_p :: M1.Parser a (a, a)
@@ -52,6 +57,9 @@ mt_p = (,) <$> M1.item <*> M1.item
 mt_q :: Eq a => a -> M1.Parser a (a, a)
 mt_q x = M1.item >>= (\y -> if x == y then return (x, x) else fmap ((,) y) M1.item)
 
+testM1Parser :: (Show t, Show a, Eq t, Eq a)
+             => [Char] -> M1.Parser t a -> [t] -> Maybe (a, [t])
+             -> Assertion
 testM1Parser s p ts expected =
   let parsed = M1.getParser p ts
   in assertEqual (s ++ " " ++ show ts ++ " = " ++ (show $ parsed))
@@ -82,9 +90,12 @@ testM3Parser s p ts expected =
                  expected
                  parsed
 
+multiTest :: (String -> p -> i -> o -> Assertion) -> String -> p -> [(i, o)]
+          -> Assertion
 multiTest assertion descr parser inputAndExpected =
   mapM_ (uncurry $ assertion descr parser) inputAndExpected
 
+m2TestDataValidParses :: [([Char], Either a (Maybe ([M3.AST], [Char])))]
 m2TestDataValidParses =
   [ ("a b c {define q (f x)}",
       Right $ Just ([ M1.ASymbol "a"
@@ -105,12 +116,16 @@ m2TestDataValidParses =
                     ,M1.ASymbol "end"]
                    ,""))]
 
+m2TestDataErrorReporting :: [([Char], Either [Char] (Maybe a))]
 m2TestDataErrorReporting = map m3ToM2Example m3TestDataErrorReporting
 
+m3ToM2Example :: (i, Either (e, p) (Maybe (Maybe a, t)))
+              -> (i, Either e (Maybe a))
 m3ToM2Example (input, (Left (e, _))) = (input, Left e)
 m3ToM2Example (input, (Right (Just (r, _)))) = (input, Right r)
 m3ToM2Example (input, (Right Nothing)) = (input, Right Nothing)
 
+m3TestDataErrorReporting :: [([Char], Either ([Char], M3.Pos) b)]
 m3TestDataErrorReporting =
   [ ("()", Left (M2.eAppOper M2.parserErrorDict, M3.Pos { M3.row = 1, M3.col = 2 }))
   , ("a\n (", Left (M2.eAppOper M2.parserErrorDict, M3.Pos { M3.row = 2, M3.col = 3 }))
@@ -132,25 +147,26 @@ m3TestDataErrorReporting =
   , ("a,b", Left (M2.eWoof M2.parserErrorDict, M3.Pos { M3.row = 1, M3.col = 2 }))
   ]
 
+m2ToM1Example :: (i, Either e (Maybe a)) -> (i, Maybe a)
 m2ToM1Example (input, expected) = (input, either (const Nothing) id expected)
 
 main :: IO ()
 main = hspec $ do
   describe "SimpleParser" $ do
     it "should work for some examples" $ do
-      testSimpleParser "s_p" s_p [1] $ Nothing
-      testSimpleParser "s_q 1" (s_q 1) [1] $ Just ([], (1,1))
+      testSimpleParser "s_p" s_p [1 :: Int] $ Nothing
+      testSimpleParser "s_q 1" (s_q 1) [1 :: Int] $ Just ([], (1,1))
       multiTest testSimpleParser
                 "s_q 1 <|> s_q 2"
                 (s_q 1 <|> s_q 2)
-                [ ([1], Just ([],(1,1)))
+                [ ([1 :: Int], Just ([],(1,1)))
                 , ([2], Just ([],(2,2)))
                 , ([3,4], Just ([],(3,4)))
                 , ([5], Nothing) ]
       multiTest testSimpleParser
                 "fmap (+) (s_check (==1)) <*> s_check (==3)"
                 (fmap (+) (S.check (==1) S.item) <*> S.check (==3) S.item)
-                [ ([1,2], Nothing)
+                [ ([1,2 :: Int], Nothing)
                 , ([1,3,5], Just ([5],4))
                 , ([1], Nothing)
                 , ([2], Nothing)
@@ -166,19 +182,19 @@ main = hspec $ do
                 , ("((a))b", Just ("",[Many [Many [One 'a']],One 'b'])) ]
   describe "MTParser" $ do
     it "should work for some examples" $ do
-      testM1Parser "mt_p" mt_p [1] $ Nothing
-      testM1Parser "mt_q 1" (mt_q 1) [1] $ Just ((1,1), [])
+      testM1Parser "mt_p" mt_p [1 :: Int] $ Nothing
+      testM1Parser "mt_q 1" (mt_q 1) [1 :: Int] $ Just ((1,1), [])
       multiTest testM1Parser
                 "mt_q 1 <|> mt_q 2"
                 (mt_q 1 <|> mt_q 2)
-                [ ([1], Just ((1,1),[]))
+                [ ([1 :: Int], Just ((1,1),[]))
                 , ([2], Just ((2,2),[]))
                 , ([3,4], Just ((3,4),[]))
                 , ([5], Nothing) ]
       multiTest testM1Parser
                 "fmap (+) (M1.check (==1)) <*> M1.check (==3)"
                 (fmap (+) (M1.check (==1) M1.item) <*> M1.check (==3) M1.item)
-                [ ([1,2], Nothing)
+                [ ([1,2 :: Int], Nothing)
                 , ([1,3,5], Just (4,[5]))
                 , ([1], Nothing)
                 , ([2], Nothing)
