d18f9b41bfa7431324d35c3dbbbe6a42facbfc37
Philippe Crama <pcfeb0009@gmx.com>
2019-03-22 22:58:24 +0000

    First success getting key labels from OnlyKey (nixos 18.09.2098.d4a1e8975a6 (Jellyfish))

--- onlykey-cli/example.hs
+++ onlykey-cli/example.hs
@@ -7,18 +7,24 @@
 --
 -- I adapted it to look for a specific device (optical mouse)
 
+import Control.Concurrent (threadDelay)
 import Control.Monad
+import Data.Char (chr)
 import Data.Int
 import qualified Data.ByteString as BS
 import System.HIDAPI as HID
 
-showHex :: (Integral a) => a -> String
+showHex :: Integral a => a -> String
 showHex = go ""
   where go s x | x < 16 = s ++ (hexChar x:"")
                | otherwise = let hd = x `div` 16
                                  tl = x `mod` 16
                              in go s hd ++ (hexChar tl:"")
         hexChar = ("0123456789abcdef" !!) . fromIntegral
+
+ascii :: Integral a => a -> String
+ascii x | 32 <= x && x < 127 = (chr $ fromIntegral x):""
+        | otherwise = "<" ++ showHex x ++ ">"
 
 pp :: DeviceInfo -> String
 pp di = "di:path=" ++ show (path di)
@@ -36,12 +42,25 @@ main :: IO ()
 main = withHIDAPI $ do
   dis <- enumerateAll
   forM_ dis $ \di -> do
-    case vendorId di of
-      0x15ca -> do
-           d <- openDeviceInfo di
-           forever $ do
-             bs <- HID.read d 6
-             putStr (show (fromIntegral (BS.index bs 1) :: Int8))
-             putChar ' '
-             print (fromIntegral (BS.index bs 2) :: Int8)
-      _ -> putStrLn (pp di)
+    putStrLn $ "\n----\n" ++ pp di
+    case (vendorId di, interfaceNumber di) of
+      (0x15ca, _) -> do -- Optical mouse
+        d <- openDeviceInfo di
+        forM_ (replicate 10 ()) $ \_ -> do
+          bs <- HID.read d 6
+          putStr (show (fromIntegral (BS.index bs 1) :: Int8))
+          putChar ' '
+          print (fromIntegral (BS.index bs 2) :: Int8)
+        close d
+      (0x1d50, 1) -> do -- OnlyKey
+        d <- openDeviceInfo di
+        r <- HID.write d $ BS.pack $ take 64 $ [255, 255, 255, 255, 229] ++ repeat 0
+        putStrLn $ "Write returned 0x" ++ showHex r
+        threadDelay 500000
+        bs <- HID.read d 10
+        _ <- forM [0..BS.length bs - 1] $ \i -> do
+               putStr $ ascii $ BS.index bs i
+               putChar ' '
+        putStrLn "..."
+        close d
+      _ -> putStrLn "NOP"
