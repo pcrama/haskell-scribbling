ef71fb30ed69861fe8eb38bc50d876d818bb671b
Philippe Crama <pcfeb0009@gmx.com>
2020-03-15 17:45:11 +0100

    Add shell.nix from 'cabal2-nix --shell .'

---
+++ queryHIBPwned/shell.nix
@@ -0,0 +1,38 @@
+# enforce "ghc844" instead of "default" to be compatible with the
+# narrow version bounds of the libraries in the cabal file (this
+# project is sometimes also developed on an Ubuntu machine where this
+# is still the GHC)
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc844", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, bytestring, hspec, megaparsec, mtl, SHA
+      , stdenv, text
+      }:
+      mkDerivation {
+        pname = "queryHIBPwned";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [ base bytestring megaparsec SHA text ];
+        executableHaskellDepends = [ base bytestring megaparsec SHA text ];
+        testHaskellDepends = [ base bytestring hspec megaparsec mtl text ];
+        homepage = "https://github.com/pcrama/haskell-scribbling/queryHIBPwned";
+        description = "Query HaveIBeenPwned with information parsed from .netrc";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
