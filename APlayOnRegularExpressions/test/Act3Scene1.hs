a8fb63296d7867b84a928c0096b264c9d3e145d9
Philippe Crama <pcfeb0009@gmx.com>
2020-07-24 12:20:55 +0000

    Add 2 other context free grammar examples to tests

--- APlayOnRegularExpressions/test/Act3Scene1.hs
+++ APlayOnRegularExpressions/test/Act3Scene1.hs
@@ -27,16 +27,48 @@ spec = describe "A Play on Regular Expressions, Act 3 Scene 1" $ do
         test "abc" True
         test "aabc" False
         test "abcc" False
-    context "works on a context-free grammar: forall n: a{n}b{n}" $
-      let anbn = epsS `altS` ((a `seqS` anbn) `seqS` b)
-          test = testMatchS anbn in do
-        test "" True
-        test "c" False
-        test "a" False
-        test "ab" True
-        test "acb" False
-        test "acbb" False
-        test "aabb" True
-        test "abba" False
-        test "baabb" False
+    context "works on context-free grammars:" $ do
+      context "forall n: a{n}b{n}" $
+        let anbn = epsS `altS` ((a `seqS` anbn) `seqS` b)
+            test = testMatchS anbn in do
+          test "" True
+          test "c" False
+          test "a" False
+          test "ab" True
+          test "acb" False
+          test "acbb" False
+          test "aabb" True
+          test "abba" False
+          test "baabb" False
+      context "forall n: a{n}b{n}c{n}" $
+        let repN 0 _ = epsS
+            repN 1 r = r
+            repN n r = (repN half r) `seqS` (repN (n - half) r)
+              where half = n `div` (2 :: Int)
+            bn n = repN n b
+            cn n = repN n c
+            abncn n = a `seqS` ((bn n `seqS` cn n) `altS` abncn (n + (1 :: Int)))
+            test = testMatchS $ epsS `altS` abncn 1 in do
+          test "" True
+          test "a" False
+          test "abc" True
+          test "aaaaabbbbbbccccccc" False
+          test "aaaaaaaaaabbbbbbbbbbcccccccccc" True
+          test "aaaaaaaaaabbbbbbbbbbcccccccccca" False
+      context "balanced parens" $
+        let left = symS (=='(')
+            right = symS (==')')
+            notParen = repS $ symS $ \x -> x /= '(' && x /= ')'
+            paren = repS $ notParen `seqS` (epsS `altS` ((left `seqS` paren `seqS` right) `seqS` notParen))
+            test = testMatchS paren in do
+          test "" True
+          test "a" True
+          test "a(" False
+          test "a()" True
+          test "a(b)" True
+          test "a(b)c" True
+          test ")(" False
+          test "(((())))" True
+          test "((((())))" False
+          test "((((a(b)c((d)e)f(((g)h(i)j))k)))lm())" True
     spec_matchSameWithAndWithoutCaching $ matchS . mxToS
